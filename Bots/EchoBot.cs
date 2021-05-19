b0a91771103646a9791bf92f61800876d8eb217f
Erickcr <Erick-96-17@hotmail.com>
2021-05-19 11:59:25 -0500

    Update EchoBot.cs

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -19,7 +19,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hola, Bienvenido!";
+            var welcomeText = "Hola, Bien!";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
